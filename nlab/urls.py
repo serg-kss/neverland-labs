4971e34acf3c65eb69b139377a5c802a1ebe92ee
GetApple <getapple@MacBook-Air-KSS.local>
2026-01-29 14:04:48 +0200

    third deploy-2

--- nlab/urls.py
+++ nlab/urls.py
@@ -1,35 +1,22 @@
 from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.i18n import i18n_patterns
-from django.conf.urls.static import static
-from django.conf import settings
 from django.shortcuts import redirect
 from django.views.i18n import set_language
+from django.utils.translation import get_language
 
 
-# --- root redirect: / -> /en/ or /uk/ ---
 def root_redirect(request):
-    return redirect(f'/{request.LANGUAGE_CODE}/')
+    lang = get_language() or "en"
+    return redirect(f"/{lang}/")
 
 
 urlpatterns = [
-    # redirect from root
     path("", root_redirect),
-
-    # admin without language prefix
     path("admin/", admin.site.urls),
     path("i18n/setlang/", set_language, name="set_language"),
-
 ]
 
-# --- i18n URLs ---
 urlpatterns += i18n_patterns(
     path("", include(("pages.urls", "pages"), namespace="pages")),
 )
-
-# --- media in DEBUG ---
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
-    )
